fe9deec07ff5994f1d38aad1c371b3277cfea011
yuchanmo <mojjijji@gmail.com>
2019-01-30 21:15:10 +0900

    create main.py file., will update code

---
+++ main.py
@@ -0,0 +1,36 @@
+# 실행예시
+# 가 - A, D, G
+# 나 - B, E, H
+# 다 - C, F,  I, J
+
+
+# A - 1. Print all universities
+# A - 2. Print all students
+# B - 3. Insert a new university
+# B - 4. Remove a university
+# C - 5. Insert a new student
+# C - 6. Remove a student
+# D - 7. Make a application
+# E - 8. Print all students who applied for a university
+# F - 9. Print all universities a students applied for
+# G - 10. Print expected successful applicants of a university
+# H - 11. Print universities expected to accept a student
+# I - 12. Exit
+# J - 13. Reset database
+
+
+
+# (1/30) Term Project
+# Due Date: 2019/02/15(Fri) 11:59PM
+# 제출: lecture@europa.snu.ac.kr
+# 파일명: PRJ_조이름.zip (예: PRJ_12조.zip)
+# 메일 제목: [DB Project] 조이름 – 조원 이름1, 조원 이름2, 조원 이름3 (예: [DB Project] 12조 – 홍길동, 김철수, 김영희)
+# 제출 내용 (압축하여 zip으로 제출)
+# 1. Runnable python script 파일 (파일명: main.py)
+# 2. DB 관련 정보
+# 2-1. DB 테이블 스키마 (CREATE 문)
+# 2-2. DB 접속 정보 (main.py에 잘 적어서 제출해 주세요.)
+# 반드시 truncate한 후 제출해주세요!
+# 3. 리포트 (pdf 포맷으로 제출해주세요!)
+# 파일명: PRJ_조이름.pdf (예: PRJ_12조.pdf)
+
